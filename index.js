1f21eb84ff94d226c210b8e3099c40f5acca41b8
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-02-19 15:24:37 +0530

    change the file names

---
+++ index.js
@@ -0,0 +1,159 @@
+var atoz = [
+    {
+        "id": 1,
+        "alphabet": "a",
+        "icon": "\ud83c\udf4e",
+        "text": "apple"
+    },
+    {
+        "id": 2,
+        "alphabet": "b",
+        "icon": "\u26bd",
+        "text": "ball"
+    },
+    {
+        "id": 3,
+        "alphabet": "c",
+        "icon": "\ud83d\udc08",
+        "text": "cat"
+    },
+    {
+        "id": 4,
+        "alphabet": "d",
+        "icon": "\ud83d\udc15",
+        "text": "dog"
+    },
+    {
+        "id": 5,
+        "alphabet": "e",
+        "icon": "\ud83e\udd5a",
+        "text": "egg"
+    },
+    {
+        "id": 6,
+        "alphabet": "f",
+        "icon": "\ud83d\udc1f",
+        "text": "fish"
+    },
+    {
+        "id": 7,
+        "alphabet": "g",
+        "icon": "\ud83d\udc10",
+        "text": "goat"
+    },
+    {
+        "id": 8,
+        "alphabet": "h",
+        "icon": "\ud83d\udc0e",
+        "text": "horse"
+    },
+    {
+        "id": 9,
+        "alphabet": "i",
+        "icon": "\ud83c\udf68",
+        "text": "ice cream"
+    },
+    {
+        "id": 10,
+        "alphabet": "j",
+        "icon": "\ud83c\udccf",
+        "text": "joker"
+    },
+    {
+        "id": 11,
+        "alphabet": "k",
+        "icon": "\ud83e\ude81",
+        "text": "kite"
+    },
+    {
+        "id": 12,
+        "alphabet": "l",
+        "icon": "\ud83c\udf4b",
+        "text": "lemon"
+    },
+    {
+        "id": 13,
+        "alphabet": "m",
+        "icon": "\ud83e\udd6d",
+        "text": "mango"
+    },
+    {
+        "id": 14,
+        "alphabet": "n",
+        "icon": "👃",
+        "text": "nose"
+    },
+    {
+        "id": 15,
+        "alphabet": "o",
+        "icon": "\ud83c\udf4a",
+        "text": "orange"
+    },
+    {
+        "id": 16,
+        "alphabet": "p",
+        "icon": "\ud83e\udd9a",
+        "text": "peacock"
+    },
+    {
+        "id": 17,
+        "alphabet": "q",
+        "icon": "\ud83d\udc78",
+        "text": "queen"
+    },
+    {
+        "id": 18,
+        "alphabet": "r",
+        "icon": "\ud83d\udc07",
+        "text": "rabbit"
+    },
+    {
+        "id": 19,
+        "alphabet": "s",
+        "icon": "\u2600\ufe0f",
+        "text": "sun"
+    },
+    {
+        "id": 20,
+        "alphabet": "t",
+        "icon": "\ud83d\udc05",
+        "text": "tiger"
+    },
+    {
+        "id": 21,
+        "alphabet": "u",
+        "icon": "\u2602\ufe0f",
+        "text": "umbrella"
+    },
+    {
+        "id": 22,
+        "alphabet": "v",
+        "icon": "\ud83c\udfbb",
+        "text": "violin"
+    },
+    {
+        "id": 23,
+        "alphabet": "w",
+        "icon": "\u231a",
+        "text": "watch"
+    },
+    {
+        "id": 24,
+        "alphabet": "x",
+        "icon": "\ud83c\udf84",
+        "text": "xmas tree"
+    },
+    {
+        "id": 25,
+        "alphabet": "y",
+        "icon": "\ud83d\udfe1",
+        "text": "yellow"
+    },
+    {
+        "id": 26,
+        "alphabet": "z",
+        "icon": "\ud83e\udd93",
+        "text": "zebra"
+    }
+];
+module.exports.atoz = atoz;
